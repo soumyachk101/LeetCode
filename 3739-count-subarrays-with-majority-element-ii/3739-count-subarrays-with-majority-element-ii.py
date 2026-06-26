e4d664760d4fc20104cbf0e51b535413a0c1a9a4
Soumya Chakraborty <soumya.chk101@gmail.com>
2026-06-26 17:43:27 +0530

    Time: 53 ms (97.3%), Space: 33.3 MB (43.24%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -1,33 +1,21 @@
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        bit = [0] * (2 * n + 2)
-        
-        def add(idx: int, val: int):
-            while idx < len(bit):
-                bit[idx] += val
-                idx += idx & (-idx)
-                
-        def query(idx: int) -> int:
-            res = 0
-            while idx > 0:
-                res += bit[idx]
-                idx -= idx & (-idx)
-            return res
+        freq = [0] * (2 * n + 1)
+        freq[n] = 1
+        i = n
+        res = 0
+        pref = 0
 
-        offset = n + 1
-        add(offset, 1)
-        
-        ans = 0
-        curr_sum = 0
-        
         for num in nums:
             if num == target:
-                curr_sum += 1
+                pref += freq[i]
+                i += 1
             else:
-                curr_sum -= 1
-            
-            ans += query(curr_sum + offset - 1)
-            add(curr_sum + offset, 1)
-            
-        return ans
+                i -= 1
+                pref -= freq[i]
+
+            freq[i] += 1
+            res += pref
+        
+        return res
