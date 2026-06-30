f4908280875c6b02d9122929bb4ba2f7645ac9b6
Soumya Chakraborty <soumya.chk101@gmail.com>
2026-06-30 13:18:43 +0530

    Time: 6 ms (100%), Space: 32.5 MB (15.25%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,10 +1,22 @@
-class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        last_seen = [-1, -1, -1]
-        count = 0
+import numpy
+
+ids = bytearray(256)
+ids[98:100] = 1, 2
+
+def numberOfSubstrings(
+    s: str, ids=bytes(ids),
+    indices=numpy.arange(1, 50_001, dtype=numpy.uint16)
+) -> int:
+    s = numpy.frombuffer(s.encode().translate(ids), dtype=numpy.uint8)
+    places = numpy.zeros((3, s.size + 1), dtype=numpy.uint16)
+    indices = indices[:s.size]
+    places[s, indices] = indices
+    return numpy.maximum.accumulate(places, axis=1, out=places)\
+        .min(axis=0)\
+        .sum()\
+        .item()    
+    
+Solution = repeat(namedtuple('Solution', ('numberOfSubstrings',))(
+    numberOfSubstrings
+)).__next__
         
-        for i, char in enumerate(s):
-            last_seen[ord(char) - 97] = i
-            count += min(last_seen) + 1
-            
-        return count
